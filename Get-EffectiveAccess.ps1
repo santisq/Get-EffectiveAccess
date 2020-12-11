27ebaf5726bb16e0e7e8dd80b14e2b6f5eda26b9
santisq <squarzon.santiago@gmail.com>
2020-12-11 11:18:36 -0300

    updated code

--- Get-EffectiveAccess.ps1
+++ Get-EffectiveAccess.ps1
@@ -12,13 +12,12 @@ Excel file or GridView (OutGrid switch).
 
 .EXAMPLE
 Pipeline examples:
-    Get-ADOrganizationalUnit -Filter {Name -eq 'Web'} | Get-EffectiveAccess
-    Get-ADComputer -Filter 'Name -like "VRTVA*"' -SearchBase 'OU=Other,OU=Azure,OU=CIO Segment Servers,DC=dir,DC=svc,DC=accenture,DC=com' | Get-EffectiveAccess 
-    Get-ADOrganizationalUnit -SearchBase 'OU=IAM,DC=dir,DC=svc,DC=accenture,DC=com' -SearchScope Subtree -Filter * | Get-EffectiveAccess 
+    Get-ADOrganizationalUnit -Filter {Name -eq 'ExampleOU'} | Get-EffectiveAccess
+    Get-ADComputer -Filter 'Name -like "ExampleCOMP*"' -SearchBase 'OU=ExampleOU,DC=domainName,DC=com' | Get-EffectiveAccess 
 
 There are 3 available switches: IncludeIAM, IncludeOprhan & OutGrid:
     IncludeIAM: To include all groups managed by IAM team.
-    IncludeOrphan: Any IdentityReference in the ACL that begins with "S-1-*" – If you want to display them use these switch.
+    IncludeOrphan: Any IdentityReference in the ACL that begins with "S-1-*" – If you want to display them use this switch.
     OutGrid: The function will use Out-GridView instead of exporting to Excel.
 
 #Requires -Modules ActiveDirectory, ImportExcel
