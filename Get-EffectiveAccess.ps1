dd7ee2ad374fb6ae15693b856a54b14d7cd3f253
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-02-23 18:17:38 -0300

    Update Get-EffectiveAccess.ps1

--- Get-EffectiveAccess.ps1
+++ Get-EffectiveAccess.ps1
@@ -1,102 +1,83 @@
-Function Get-EffectiveAccess {
+# requires -Modules ActiveDirectory
+$ErrorActionPreference = 'Stop'
+
+function Get-EffectiveAccess {
 [CmdletBinding()]
 param(
-    [Parameter(
-        Mandatory,
-        ValueFromPipelineByPropertyName
-    )]
-    [ValidatePattern(
-        '(?:(CN=([^,]*)),)?(?:((?:(?:CN|OU)=[^,]+,?)+),)?((?:DC=[^,]+,?)+)$'
-    )][string]$DistinguishedName,
-    [switch]$IncludeOrphan
+[Parameter(
+    Mandatory,
+    ValueFromPipelineByPropertyName
+)]
+[ValidatePattern(
+    '(?:(CN=([^,]*)),)?(?:((?:(?:CN|OU)=[^,]+,?)+),)?((?:DC=[^,]+,?)+)$'
+)][string]$DistinguishedName,
+[switch]$IncludeOrphan
 )
 
-    begin
-    {
-        # requires -Modules ActiveDirectory
-        $ErrorActionPreference = 'Stop'
+    begin {
         $GUIDMap = @{}
         $domain = Get-ADRootDSE
         $z = '00000000-0000-0000-0000-000000000000'
         $hash = @{
-            SearchBase = $domain.schemaNamingContext
-            LDAPFilter = '(schemaIDGUID=*)'
-            Properties = 'name','schemaIDGUID'
+            SearchBase  = $domain.schemaNamingContext
+            LDAPFilter  = '(schemaIDGUID=*)'
+            Properties  = 'name', 'schemaIDGUID'
             ErrorAction = 'SilentlyContinue'
         }
         $schemaIDs = Get-ADObject @hash 
 
         $hash = @{
-            SearchBase = "CN=Extended-Rights,$($domain.configurationNamingContext)"
-            LDAPFilter = '(objectClass=controlAccessRight)'
-            Properties = 'name','rightsGUID'
+            SearchBase  = "CN=Extended-Rights,$($domain.configurationNamingContext)"
+            LDAPFilter  = '(objectClass=controlAccessRight)'
+            Properties  = 'name', 'rightsGUID'
             ErrorAction = 'SilentlyContinue'
         }
         $extendedRigths = Get-ADObject @hash
 
-        foreach($i in $schemaIDs)
-        {
-            if(-not $GUIDMap.ContainsKey([System.GUID]$i.schemaIDGUID))
-            {
-                $GUIDMap.add([System.GUID]$i.schemaIDGUID,$i.name)
+        foreach($i in $schemaIDs) {
+            if(-not $GUIDMap.ContainsKey([System.GUID]$i.schemaIDGUID)) {
+                $GUIDMap.add([System.GUID]$i.schemaIDGUID, $i.name)
             }
         }
-        foreach($i in $extendedRigths)
-        {
-            if(-not $GUIDMap.ContainsKey([System.GUID]$i.rightsGUID))
-            {
-                $GUIDMap.add([System.GUID]$i.rightsGUID,$i.name)
+        foreach($i in $extendedRigths) {
+            if(-not $GUIDMap.ContainsKey([System.GUID]$i.rightsGUID)) {
+                $GUIDMap.add([System.GUID]$i.rightsGUID, $i.name)
             }
         }
     }
 
-    process
-    {
-        $result = [system.collections.generic.list[pscustomobject]]::new()
+    process {
         $object = Get-ADObject $DistinguishedName
         $acls = (Get-ACL "AD:$object").Access
         
-        foreach($acl in $acls)
-        {
-            
-            $objectType = if($acl.ObjectType -eq $z)
-            {
+        $result = foreach($acl in $acls) {
+            $objectType = (
+                $GUIDMap[$acl.ObjectType],
                 'All Objects (Full Control)'
-            }
-            else
-            {
-                $GUIDMap[$acl.ObjectType]
-            }
-
-            $inheritedObjType = if($acl.InheritedObjectType -eq $z)
-            {
+            )[$acl.ObjectType -eq $z]
+            
+            $inheritedObjType = (
+                $GUIDMap[$acl.InheritedObjectType],
                 'Applied to Any Inherited Object'
-            }
-            else
-            {
-                $GUIDMap[$acl.InheritedObjectType]
-            }
+            )[$acl.InheritedObjectType -eq $z]
 
-            $result.Add(
-                [PSCustomObject]@{
-                    Name = $object.Name
-                    IdentityReference = $acl.IdentityReference
-                    AccessControlType = $acl.AccessControlType
-                    ActiveDirectoryRights = $acl.ActiveDirectoryRights
-                    ObjectType = $objectType
-                    InheritedObjectType = $inheritedObjType
-                    InheritanceType = $acl.InheritanceType
-                    IsInherited = $acl.IsInherited
-            })
+            [PSCustomObject]@{
+                Name = $object.Name
+                IdentityReference = $acl.IdentityReference
+                AccessControlType = $acl.AccessControlType
+                ActiveDirectoryRights = $acl.ActiveDirectoryRights
+                ObjectType = $objType
+                InheritedObjectType = $inheritedObjType
+                InheritanceType = $acl.InheritanceType
+                IsInherited = $acl.IsInherited
+            }
         }
         
-        if(-not $IncludeOrphan.IsPresent)
-        {
-            $result | Sort-Object IdentityReference |
-            Where-Object {$_.IdentityReference -notmatch 'S-1-*'}
-            return
+        if($IncludeOrphan.IsPresent) {
+            return $result | Sort-Object IdentityReference
         }
-
-        return $result | Sort-Object IdentityReference
+        
+        $result | Where-Object { -not $_.IdentityReference.StartsWith('S-1-5-21') } |
+        Sort-Object IdentityReference        
     }
 }
