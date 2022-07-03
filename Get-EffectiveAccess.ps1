77c0b6f36a9e611d6629df12a28a15053eca7294
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-07-03 12:10:41 -0300

    function update, doc update

--- Get-EffectiveAccess.ps1
+++ Get-EffectiveAccess.ps1
@@ -1,31 +1,23 @@
-# requires -Modules ActiveDirectory
-$ErrorActionPreference = 'Stop'
-
 function Get-EffectiveAccess {
-[CmdletBinding()]
-param(
-    [Parameter(
-        Mandatory,
-        ValueFromPipelineByPropertyName
-    )]
-    [ValidatePattern(
-        '(?:(CN=([^,]*)),)?(?:((?:(?:CN|OU)=[^,]+,?)+),)?((?:DC=[^,]+,?)+)$'
-    )]
-    [string]$DistinguishedName,
-    [switch]$IncludeOrphan
-)
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidatePattern('(?:(CN=([^,]*)),)?(?:((?:(?:CN|OU)=[^,]+,?)+),)?((?:DC=[^,]+,?)+)$')]
+        [alias('ObjectGUID','ObjectSID','DistinguishedName','sAMAccountName')]
+        [string] $Identity
+    )
 
     begin {
         $GUIDMap = @{}
-        $domain = Get-ADRootDSE
-        $z = '00000000-0000-0000-0000-000000000000'
-        $hash = @{
+        $domain  = Get-ADRootDSE
+        $guid    = [guid]::new([byte[]]::new(16))
+        $hash    = @{
             SearchBase  = $domain.schemaNamingContext
             LDAPFilter  = '(schemaIDGUID=*)'
             Properties  = 'name', 'schemaIDGUID'
             ErrorAction = 'SilentlyContinue'
         }
-        $schemaIDs = Get-ADObject @hash 
+        $schemaIDs = Get-ADObject @hash
 
         $hash = @{
             SearchBase  = "CN=Extended-Rights,$($domain.configurationNamingContext)"
@@ -36,49 +28,57 @@ param(
         $extendedRigths = Get-ADObject @hash
 
         foreach($i in $schemaIDs) {
-            if(-not $GUIDMap.ContainsKey([System.GUID]$i.schemaIDGUID)) {
-                $GUIDMap.add([System.GUID]$i.schemaIDGUID, $i.name)
+            if(-not $GUIDMap.ContainsKey([guid] $i.schemaIDGUID)) {
+                $GUIDMap.Add([guid] $i.schemaIDGUID, $i.name)
             }
         }
         foreach($i in $extendedRigths) {
-            if(-not $GUIDMap.ContainsKey([System.GUID]$i.rightsGUID)) {
-                $GUIDMap.add([System.GUID]$i.rightsGUID, $i.name)
+            if(-not $GUIDMap.ContainsKey([guid] $i.rightsGUID)) {
+                $GUIDMap.Add([guid] $i.rightsGUID, $i.name)
             }
         }
     }
 
     process {
-        $object = Get-ADObject $DistinguishedName
-        $acls = (Get-ACL "AD:$object").Access
-        
-        $result = foreach($acl in $acls) {
-            $objectType = (
-                $GUIDMap[$acl.ObjectType],
-                'All Objects (Full Control)'
-            )[$acl.ObjectType -eq $z]
-            
-            $inheritedObjType = (
-                $GUIDMap[$acl.InheritedObjectType],
-                'Applied to Any Inherited Object'
-            )[$acl.InheritedObjectType -eq $z]
+        try {
+            $object = Get-ADObject $DistinguishedName
+            $acls   = (Get-ACL "AD:$object").Access
 
-            [PSCustomObject]@{
-                Name                  = $object.Name
-                IdentityReference     = $acl.IdentityReference
-                AccessControlType     = $acl.AccessControlType
-                ActiveDirectoryRights = $acl.ActiveDirectoryRights
-                ObjectType            = $objectType
-                InheritedObjectType   = $inheritedObjType
-                InheritanceType       = $acl.InheritanceType
-                IsInherited           = $acl.IsInherited
+            foreach($acl in $acls) {
+                if($guid.Equals($acl.ObjectType)) {
+                    $objectType = 'All Objects (Full Control)'
+                }
+                elseif($GUIDMap.ContainsKey($acl.ObjectType)) {
+                    $objectType = $GUIDMap[$acl.ObjectType]
+                }
+                else {
+                    $objectType = $acl.ObjectType
+                }
+
+                if($guid.Equals($acl.InheritedObjectType)) {
+                    $inheritedObjType = 'Applied to Any Inherited Object'
+                }
+                elseif($GUIDMap.ContainsKey($acl.InheritedObjectType)) {
+                    $inheritedObjType = $GUIDMap[$acl.InheritedObjectType]
+                }
+                else {
+                    $inheritedObjType = $acl.InheritedObjectType
+                }
+
+                [PSCustomObject]@{
+                    Name                  = $object.Name
+                    IdentityReference     = $acl.IdentityReference
+                    AccessControlType     = $acl.AccessControlType
+                    ActiveDirectoryRights = $acl.ActiveDirectoryRights
+                    ObjectType            = $objectType
+                    InheritedObjectType   = $inheritedObjType
+                    InheritanceType       = $acl.InheritanceType
+                    IsInherited           = $acl.IsInherited
+                }
             }
         }
-        
-        if($IncludeOrphan.IsPresent) {
-            return $result | Sort-Object IdentityReference
+        catch {
+            $PSCmdlet.WriteError($_)
         }
-        
-        $result | Where-Object { -not $_.IdentityReference.StartsWith('S-1-5-21') } |
-        Sort-Object IdentityReference        
     }
 }
--- README.md
+++ README.md
@@ -1,22 +1,43 @@
 # Get-EffectiveAccess
 
-### DESCRIPTION
-Gets the ACL of an object and translate it to readable data.
+## Description
 
-### USAGE
-```
-PS \> Get-ADOrganizationalUnit -Filter "Name -eq 'ExampleOU'" | Get-EffectiveAccess | Out-GridView
-PS \> Get-EffectiveAccess -DistinguishedName 'OU=ExampleOU,DC=domainName,DC=com' | Out-GridView
-PS \> $effectiveAccess = Get-ADGroup exampleGroup | Get-EffectiveAccess -IncludeOrphan
-PS \> Get-ADOrganizationalUnit -Filter * | Select -First 10 | Get-EffectiveAccess | Out-GridView
-```
-### SWITCH
-- **`IncludeOrphan`** By default, the function will filter all orphaned ACLs. Use this switch to include all `IdentityReference` that begin with `S-1-*`
+PowerShell function that tries to give a friendly translatation of [`Get-Acl`](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-acl?view=powershell-7.2) into human readable data. The function is designed exclusively for Active Directory, and requires the [__ActiveDirectory Module__](https://docs.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps).
 
-### Requirements
-- PowerShell v5.1
+## Examples
+
+
+- Get the _Effective Access_ of the Organizational Unit named `ExampleOU`:
+
+```powershell
+Get-ADOrganizationalUnit -Filter "Name -eq 'ExampleOU'" |
+    Get-EffectiveAccess | Out-GridView
+```
+
+- Same as above but using the OU's `DistinguishedName` attribute:
+
+```powershell
+Get-EffectiveAccess -Identity 'OU=ExampleOU,DC=domainName,DC=com' | Out-GridView
+```
+
+- Store the _Effective Access_ of the group named `exampleGroup` in a variable:
+
+```powershell
+$effectiveAccess = Get-ADGroup exampleGroup | Get-EffectiveAccess
+```
+
+- Get the _Effective Access_ of the first 10 OUs found in the Domain:
+
+```powershell
+Get-ADOrganizationalUnit -Filter * | Select -First 10 |
+    Get-EffectiveAccess | Out-GridView
+```
+
+## Sample output with `Out-GridView`
+
+![exampleoutput](/Screenshot/effectiveAccess.png?raw=true)
+
+## Requirements
+
+- PowerShell v5.1+
 - ActiveDirectory PS Module
-
-### EXAMPLE OUTPUT WITH Out-GridView
-
-![exampleoutput](/effectiveAccess.png?raw=true)
